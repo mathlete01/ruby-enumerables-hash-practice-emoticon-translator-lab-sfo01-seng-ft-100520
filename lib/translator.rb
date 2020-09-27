dbb896c06e6ae7dcbfa5fe2c2488e3d72731f6af
Matthew Sallin <msallin@gmail.com>
2020-09-27 00:53:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,6 +8,8 @@ def load_library(path)
   emoticons.each do |key, value|
     #puts "***** #{key}: #{value}"
     newHash[key] = {}
+    newHash[key][:english] = ""
+    newHash[key][:japanese] = ""
     #binding.pry
   end
   newHash
