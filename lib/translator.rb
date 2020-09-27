eaf5aac5f12e51f9fe928db953f2cdaa57a5bb26
Matthew Sallin <msallin@gmail.com>
2020-09-27 00:39:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(path)
   emoticons = YAML.load_file(path)
   emoticons.each do |key, value|
     #puts "***** #{key}: #{value}"
-    #newHash[key][value]
+    newHash[key] = {}
     #puts "***** newHash = #{newHash}"
     binding.pry
   end
