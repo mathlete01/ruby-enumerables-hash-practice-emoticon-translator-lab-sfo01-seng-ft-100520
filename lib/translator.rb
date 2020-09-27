fafb9ba1ead5920f7d6a5cfa61aa0e9f8ef5d407
Matthew Sallin <msallin@gmail.com>
2020-09-27 01:08:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,14 +6,11 @@ def load_library(path)
   newHash = {}
   emoticons = YAML.load_file(path)
   emoticons.each do |key, value|
-    #puts "***** #{key}: #{value}"
     newHash[key] = {}
     newHash[key][:english] = value[0]
     newHash[key][:japanese] = value[1]
-    #binding.pry
   end
   newHash
-  #binding.pry
 end
 
 def get_japanese_emoticon
